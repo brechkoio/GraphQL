953f285125fad46cc1ef632971f7e8c7836397d8
Brechko Yurii <brechkoio@gmail.com>
2023-01-05 10:54:30 +0200

    schemas and resolvers

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3001, () => {
     console.log(`Server started on port: 3001`);
